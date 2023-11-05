0cb4c584ef64845f564438e5a7643d4c761ec0cc
Aleksey <a.e.sokolkov@gmail.com>
2023-11-05 17:16:26 +0500

    fix: translator client cache

--- ObjectsLoader/Clients/TranslatorClient.cs
+++ ObjectsLoader/Clients/TranslatorClient.cs
@@ -1,16 +1,31 @@
 ﻿using System.Net;
+using ObjectsLoader.Services;
 
 namespace ObjectsLoader.Clients;
 
 public class TranslatorClient
 {
-    private const string Url = "https://localhost:7001/translate?text={0}&target=ru&source={1}";
+    private const string Target = "ru";
+    private const string Url = "https://localhost:7001/translate?text={0}&target={1}&source={2}";
     private readonly HttpClient client = new();
+    private readonly DistributedCache cache = new();
 
     public async Task<string?> Fetch(string text, string? source = null)
     {
-        var query = string.Format(Url, text, source);
+        var key = $"{Target}|{text}";
+        var translation = await cache.GetAsync(key);
+        if (translation is not null)
+        {
+            return translation;
+        }
+        var query = string.Format(Url, text, Target, source);
         var response = await client.GetAsync(query);
-        return response.StatusCode == HttpStatusCode.OK ? await response.Content.ReadAsStringAsync() : null;
+        if (response.StatusCode != HttpStatusCode.OK)
+        {
+            return null;
+        }
+        var stringResponse = await response.Content.ReadAsStringAsync();
+        await cache.SetAsync(key, stringResponse);
+        return stringResponse;
     }
 }
---
+++ ObjectsLoader/Services/DistributedCache.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+
+namespace ObjectsLoader.Services;
+
+public class DistributedCache
+{
+    private readonly MemoryCache cache = new(new MemoryCacheOptions());
+    
+    public string? Get(string key)
+    {
+        cache.TryGetValue<string>(key, out var result);
+        return result;
+    }
+
+    public async Task<string?> GetAsync(string key)
+    {
+        return await Task.FromResult(Get(key));
+    }
+
+    public void Set(string key, string value)
+    {
+        cache.Set(key, value);
+    }
+
+    public async Task SetAsync(string key, string value)
+    {
+        Set(key, value);
+        await Task.CompletedTask;
+    }
+
+}
