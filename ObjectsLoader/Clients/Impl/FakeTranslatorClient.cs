8aaf26bdc2113f349b7632dd5129074a175a2f9b
Aleksey <a.e.sokolkov@gmail.com>
2023-12-22 00:21:56 +0500

    feat: fake translator for tests

---
+++ ObjectsLoader/Clients/Impl/FakeTranslatorClient.cs
@@ -0,0 +1,9 @@
+﻿namespace ObjectsLoader.Clients.Impl;
+
+public class FakeTranslatorClient : ITranslatorClient
+{
+    public Task<string?> Fetch(string text, string target, string? source = null)
+    {
+        return Task.FromResult(text);
+    }
+}
